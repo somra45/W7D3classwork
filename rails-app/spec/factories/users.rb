dd44f0b0b3d73bfc82de106f81af5e2eda9548a0
Ethanjonm <ethanjonmercado@gmail.com>
2023-04-19 14:42:09 -0400

    started user_spec.rb

---
+++ rails-app/spec/factories/users.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  session_token   :string           not null
+#  password_digest :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+FactoryBot.define do
+  factory :user do
+    username { Faker::Movies::LordOfTheRings.character }
+    password { 'password' }
+
+    factory :Legolas do 
+      username { 'Legolas' }
+    end
+  end
+end
---
+++ rails-app/spec/models/user_spec.rb
@@ -0,0 +1,65 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  session_token   :string           not null
+#  password_digest :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+
+  it { should validate_presence_of(:username) }
+  it { should validate_presence_of(:password_digest) }
+  it { should validate_presence_of(:session_token) }
+  it { should validate_length_of(:password).is_at_least(6)}
+
+  describe 'uniqueness' do
+    before :each do 
+        create(:user)
+    end
+    
+    it { should validate_uniqueness_of(:username) }
+    it { should validate_uniqueness_of(:session_token) }
+  end
+
+  describe 'is_password?' do 
+    let(:user) { create(:user) }
+
+    context "with a vaild password" do 
+        it "should return true" do 
+            expect(user.is_password?('password')).to be true
+        end
+    end
+
+    context "with an invalid password" do 
+        it "should return false" do 
+            expect(user.is_password?('passowrd')).to be false
+        end
+    end
+
+end
+
+describe "password security" do 
+
+    it 'does not save password to the database' do 
+        FactoryBot.create(:legolas)
+        user = User.find_by(username: 'Legolas')
+        expect(user.password).not_to eq('password')
+    end
+
+    it 'secures password using BCrypt' do 
+        expect(BCrypt::Password).to receive(:create).with('cheezit')
+
+        FactoryBot.build(:user, password: 'cheezit')
+    end
+
+end
+
+end
